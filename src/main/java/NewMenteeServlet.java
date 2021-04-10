0c1053be546f4dc50411a7af7012dbe90b921a80
Jacqueline <jacquelinetapia8@gmail.com>
2021-04-10 20:25:42 +0000

    Change placement of url redirection

--- src/main/java/NewMenteeServlet.java
+++ src/main/java/NewMenteeServlet.java
@@ -1,3 +1,5 @@
+
+import com.google.api.client.util.Key;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
@@ -11,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
-/** Servlet responsible for creating new tasks. */
+/** Servlet responsible for creating new memtees. */
 @WebServlet("/new-mentee")
 public class NewMenteeServlet extends HttpServlet {
 
@@ -28,10 +30,10 @@ public class NewMenteeServlet extends HttpServlet {
     String menteeDesiredSkills = Jsoup.clean(request.getParameter("skill-desire"), Whitelist.none());
 
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    response.sendRedirect("/matchingPage.html");
+
     KeyFactory keyFactory = datastore.newKeyFactory().setKind("Mentee");
-    FullEntity menteeEntity =
-        Entity.newBuilder(keyFactory.newKey())
+
+    FullEntity menteeEntity =Entity.newBuilder(keyFactory.newKey())
             .set("menteeName", menteeName)
             .set("menteeEmail", menteeEmail)
             .set("menteeUsername", menteeUsername)
@@ -42,6 +44,7 @@ public class NewMenteeServlet extends HttpServlet {
             .set("menteeDesiredSkills", menteeDesiredSkills)
             .build();
     datastore.put(menteeEntity);
+    response.sendRedirect("/matchingPage.html");
     
   }
 }
