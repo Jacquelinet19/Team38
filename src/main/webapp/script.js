19fcf3c272b3dd037160460336cf7a279d9d8aa0
MariferSalas <a01176943@itesm.mx>
2021-04-10 05:52:37 +0000

    Add profile page, adapt script for showing cards

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -34,56 +34,78 @@ function dummyDBMentor(name, email, username, job, college, intro, expertise) {
 }
 
 /*
-Function: addMenteeCards()
-Add list of Mentee to the feed if the username is a mentor
+Function: addMenteeCard()
+Add a Mentee card to the content div
 */
-function addMenteeCards(myMentees) {
-    for(var j=0; j<myMentees.length; j++){
-        var tag = document.createElement('div');
-        tag.innerHTML = '<div class="card">' + 
-            '<div id="card-img"><img src="images/img1.png"></img></div>' + 
-            '<div id="card-nameholder"><h3>' + myMentees[j].name + '</h3></div>' + 
-            '<div id="card-seemore-button">' + 
-                '<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#more-info' + j + '">See more</button>' + 
-            '</div>' + 
-            '<div id="more-info' + j + '" class="collapse" style="padding: 2px 120px;">' + 
-                '<strong>E-mail: </strong><p>' + myMentees[j].email + '</p>' + 
-                '<strong>School: </strong><p>' + myMentees[j].school + '</p>' + 
-                '<strong>Class Standing: </strong><p>' + myMentees[j].classStanding + '</p>' + 
-                '<strong>About me: </strong><p>' + myMentees[j].intro + '</p>' + 
-                '<strong>My objectives: </strong><p>' + myMentees[j].reason + '</p>' + 
-                '<strong>Skills Needed: </strong><p>' + myMentees[j].desiredSkills + '</p>' + 
-            '</div>' + '</div>';
+function addMenteeCard(myMentee, count, type) {
+    var tag = document.createElement('div');
+    var commonInfo = '<div class="card">' + '<div id="card-img"><img src="images/img1.png"></img></div>' + 
+        '<div id="card-nameholder"><h3>' + myMentee.name + '</h3></div>';
         
-        var element = document.getElementById("content");
-        element.appendChild(tag);
+    if(type == "feed"){
+        tag.innerHTML = commonInfo + '<div id="card-seemore-button">' + 
+            '<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#more-info' + count + '">See more</button>' + 
+        '</div>' + 
+        '<div id="more-info' + count + '" class="collapse" style="padding: 2px 120px;">' + 
+            '<strong>E-mail: </strong><p>' + myMentee.email + '</p>' + 
+            '<strong>School: </strong><p>' + myMentee.school + '</p>' + 
+            '<strong>Class Standing: </strong><p>' + myMentee.classStanding + '</p>' + 
+            '<strong>About me: </strong><p>' + myMentee.intro + '</p>' + 
+            '<strong>My objectives: </strong><p>' + myMentee.reason + '</p>' + 
+            '<strong>Skills Needed: </strong><p>' + myMentee.desiredSkills + '</p>' + 
+        '</div>' + '</div>';
     }
+    else if(type == "profile"){
+        tag.innerHTML = commonInfo + '<div id="more-info" style="padding: 2px 120px;">' + 
+            '<strong>Username: </strong><p>' + myMentee.username + '</p>' + 
+            '<strong>E-mail: </strong><p>' + myMentee.email + '</p>' + 
+            '<strong>School: </strong><p>' + myMentee.school + '</p>' + 
+            '<strong>Class Standing: </strong><p>' + myMentee.classStanding + '</p>' + 
+            '<strong>About me: </strong><p>' + myMentee.intro + '</p>' + 
+            '<strong>My objectives: </strong><p>' + myMentee.reason + '</p>' + 
+            '<strong>Skills Needed: </strong><p>' + myMentee.desiredSkills + '</p>' + 
+        '</div>' + '</div>';
+    }
+    
+    var element = document.getElementById("content");
+    element.appendChild(tag);
 }
 
 /*
-Function: addMentorCards()
-Add list of Mentors to the feed if the username is a mentee
+Function: addMentorCard()
+Add a Mentor card to the content div
 */
-function addMentorCards(myMentors) {
-    for(var j=0; j<myMentors.length; j++){
-        var tag = document.createElement('div');
-        tag.innerHTML = '<div class="card">' + 
-            '<div id="card-img"><img src="images/img1.png"></img></div>' + 
-            '<div id="card-nameholder"><h3>' + myMentors[j].name + '</h3></div>' + 
-            '<div id="card-seemore-button">' + 
-                '<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#more-info' + j + '">See more</button>' + 
-            '</div>' + 
-            '<div id="more-info' + j + '" class="collapse" style="padding: 2px 120px;">' + 
-                '<strong>E-mail: </strong><p>' + myMentors[j].email + '</p>' + 
-                '<strong>College: </strong><p>' + myMentors[j].college + '</p>' + 
-                '<strong>Job: </strong><p>' + myMentors[j].job + '</p>' + 
-                '<strong>About me: </strong><p>' + myMentors[j].intro + '</p>' + 
-                '<strong>Area of expertise: </strong><p>' + myMentors[j].expertise + '</p>' + 
-            '</div>' + '</div>';
-        
-        var element = document.getElementById("content");
-        element.appendChild(tag);
+function addMentorCard(myMentor, count, type) {
+    var tag = document.createElement('div');
+    var commonInfo = '<div class="card">' + 
+        '<div id="card-img"><img src="images/img1.png"></img></div>' + 
+        '<div id="card-nameholder"><h3>' + myMentor.name + '</h3></div>';
+    
+    if(type == "feed"){
+        tag.innerHTML = commonInfo + '<div id="card-seemore-button">' + 
+            '<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#more-info' + count + '">See more</button>' + 
+        '</div>' + 
+        '<div id="more-info' + count + '" class="collapse" style="padding: 2px 120px;">' + 
+            '<strong>E-mail: </strong><p>' + myMentor.email + '</p>' + 
+            '<strong>College: </strong><p>' + myMentor.college + '</p>' + 
+            '<strong>Job: </strong><p>' + myMentor.job + '</p>' + 
+            '<strong>About me: </strong><p>' + myMentor.intro + '</p>' + 
+            '<strong>Area of expertise: </strong><p>' + myMentor.expertise + '</p>' + 
+        '</div>' + '</div>';
     }
+    else if (type == "profile"){
+        tag.innerHTML = commonInfo + '<div id="more-info" style="padding: 2px 120px;">' + 
+            '<strong>Username: </strong><p>' + myMentor.username + '</p>' + 
+            '<strong>E-mail: </strong><p>' + myMentor.email + '</p>' + 
+            '<strong>College: </strong><p>' + myMentor.college + '</p>' + 
+            '<strong>Job: </strong><p>' + myMentor.job + '</p>' + 
+            '<strong>About me: </strong><p>' + myMentor.intro + '</p>' + 
+            '<strong>Area of expertise: </strong><p>' + myMentor.expertise + '</p>' + 
+        '</div>' + '</div>';
+    }
+    
+    var element = document.getElementById("content");
+    element.appendChild(tag);
 }
 
 /*
@@ -117,14 +139,59 @@ function addTableCards() {
     //Having a mentor username, showing list of mentees
     for(var i=0; i<myMentors.length; i++){
         if(username == myMentors[i].username){
-            addMenteeCards(myMentees);
+            for(var j=0; j<myMentees.length; j++){
+                addMenteeCard(myMentees[j], j, "feed");
+            }
+            break;
         }
     }
 
     //Having a mentee username, showing list of mentors
     for(var i=0; i<myMentees.length; i++){
         if(username == myMentees[i].username){
-            addMentorCards(myMentors);
+            for(var j=0; j<myMentors.length; j++){
+                addMentorCard(myMentors[j], j, "feed");
+            }
+            break;
+        }
+    }
+}
+
+/*
+Function: addProfile()
+Adapts the profile info to the username
+    
+Notes:
+    Up until now works in a local way with structs containing dummy info 
+    Next step is to adapt it to work with datastorage
+*/
+function addProfile() {
+    const username = "john123";
+
+    var myMentees = [
+        new dummyDBMentee('John S', 'john@myemail.com', 'john123', 'mySchool', 2021, "I'm a student", "I like to learn", "C++"),
+        new dummyDBMentee('Mary S', 'mary@myemail.com', 'mary123', 'mySchool', 2022, "I'm a student", "I like to learn", "Java"),
+        new dummyDBMentee('Nick S', 'nick@myemail.com', 'nick123', 'mySchool', 2024, "I'm a student", "I like to learn", "Go")
+    ];
+
+    var myMentors = [
+        new dummyDBMentor('John T', 'john@myemail.com', 'john124', 'Senior Software Engineer', 'MyCollege', "I'm a software engineer", "JavaScript"),
+        new dummyDBMentor('Mary T', 'mary@myemail.com', 'mary124', 'UX Designer', 'MyCollege', "I'm a UX specialist", "Accesibility")
+    ];
+
+    //Having a mentor username, show a mentor's profile POV
+    for(var i=0; i<myMentors.length; i++){
+        if(username == myMentors[i].username){
+            addMentorCard(myMentors[i], 0, "profile");
+            break;
+        }
+    }
+
+    //Having a mentee username, show a mentee's profile POV
+    for(var i=0; i<myMentees.length; i++){
+        if(username == myMentees[i].username){
+            addMenteeCard(myMentees[i], 0, "profile");
+            break;
         }
     }
 }
